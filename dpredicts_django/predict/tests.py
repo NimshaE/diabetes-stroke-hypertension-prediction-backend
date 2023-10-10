4e239255c4e77d87bed69ffce4ffcfd37b25182d
Nimsha Erin <nimsha.e@blackvt.com>
2023-10-10 10:52:41 +0530

    Unit tests -backend

--- dpredicts_django/predict/tests.py
+++ dpredicts_django/predict/tests.py
@@ -1,3 +1,33 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .serializers import InputDataSerializer
 
-# Create your tests here.
+class PredictionsViewSetTestCase(APITestCase):
+
+    def test_create_prediction(self):
+        input_data = {
+            "Age": 4,
+            "Sex": 1,
+            "HighChol": 0,
+            "BMI": 26,
+            "Smoker": 0,
+            "HeartDiseaseorAttack": 0,
+            "PhysActivity": 1,
+            "HvyAlcoholConsump": 0,
+            "GenHlth": 3,
+            "MentHlth": 5,
+        }
+
+        serializer = InputDataSerializer(data=input_data)
+        self.assertTrue(serializer.is_valid(), f"Serializer errors: {serializer.errors}")
+
+        url = reverse("predictions-list")
+        response = self.client.post(url, input_data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        self.assertEqual(response.data, {
+            "Diabetes": "No",
+            "Hypertension": "Yes",
+            "Stroke": "Yes",
+        })
